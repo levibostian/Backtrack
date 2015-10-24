87178149bb295a8bd344b12421d8bfe8d9d351cf
Levi Bostian <levi.bostian@gmail.com>
2015-10-24 16:18:45 -0500

    Create incoming endpoint and a conversation via twilio session cookies.

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var twilio = require('twilio');
 
 var twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 var twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
@@ -11,6 +12,33 @@ var client = require('twilio')(twilioAccountSid, twilioAuthToken);
 
 var app = express();
 app.use(express.static(__dirname + '/public')); // starting static fileserver
+
+app.get('/incoming',
+        function(req, res) {
+            var previousStep = req.cookie.previousStep;
+            var responseMessage = "";
+
+            if (!previousStep) {
+                responseMessage = "you're on step 1";
+                previousStep = 0;
+            } else {
+                switch (previousStep) {
+                case 1:
+                    responseMessage = "you're on step 2";
+                    break;
+                case 2:
+                    responseMessage = "you're on step 3. All done. Thanks!";
+                    break;
+                }
+            }
+            
+            var response = new twilio.TwimlResponse();
+            response.sms(responseMessage);
+            res.cookie('previousStep', previousStep + 1);
+            res.send(response.toString(), {
+                'Content-Type':'text/xml'
+            }, 200);
+        });
 
 app.get('/',
         function(req, res) {            
